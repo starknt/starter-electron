df1750ac8ed78e8f98af92bcecbc6a6da23dad60
starknt <1431880400@qq.com>
2022-08-06 23:27:34 +0800

    test fix e2e

--- tests/window.e2e.spec.ts
+++ tests/window.e2e.spec.ts
@@ -13,13 +13,34 @@ afterAll(async () => {
   await electronApplication.close()
 })
 
+test('Main window state', async () => {
+  const windowState: { isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean }
+    = await electronApplication.evaluate(({ BrowserWindow }) => {
+      const mainWindow = BrowserWindow.getAllWindows()[0]
+
+      const getState = () => ({
+        isVisible: mainWindow.isVisible(),
+        isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
+        isCrashed: mainWindow.webContents.isCrashed(),
+      })
+
+      return new Promise((resolve) => {
+        if (mainWindow.isVisible())
+          resolve(getState())
+
+        else
+          mainWindow.once('ready-to-show', () => setTimeout(() => resolve(getState()), 0))
+      })
+    })
+
+  expect(windowState.isCrashed, 'The app has crashed').toBeFalsy()
+  expect(windowState.isVisible, 'The main window was not visible').toBeTruthy()
+  expect(windowState.isDevToolsOpened, 'The DevTools panel was open').toBeFalsy()
+})
+
 test('Main window web content', async () => {
   const page = await electronApplication.firstWindow()
-  page.on('domcontentloaded', async () => {
-    const element = await page.$('#app', { strict: true })
-    expect(element, 'Was unable to find the root element').toBeDefined()
-
-    const innerHTML = await element.innerHTML()
-    expect(innerHTML.trim(), 'Window content was empty').not.equal('')
-  })
+  const element = await page.$('#app', { strict: true })
+  expect(element, 'Was unable to find the root element').toBeDefined()
+  expect((await element.innerText()).trim(), 'Window content was empty').not.equal('')
 })
