5031aae8f61bf77cf380b42fe78b204e26a329bb
starknt <1431880400@qq.com>
2022-08-06 16:58:10 +0800

    chore: clean script

--- .knt/scripts/clean.ts
+++ .knt/scripts/clean.ts
@@ -1,16 +1,22 @@
 import fs from 'fs'
-import path from 'path'
+import { join } from 'path'
 import rimraf from 'rimraf'
 import { appPath, rootPath } from '../paths'
 
-if (fs.existsSync(path.join(appPath, 'dist'))) {
-  rimraf(path.join(appPath, 'dist'), (err) => {
+if (fs.existsSync(join(appPath, 'dist'))) {
+  rimraf(join(appPath, 'dist'), (err) => {
     if (err) console.error(err)
   })
 }
 
-if (fs.existsSync(path.join(rootPath, 'release', 'build'))) {
-  rimraf(path.join(rootPath, 'release', 'build'), (err) => {
+if (fs.existsSync(join(rootPath, 'release', 'build'))) {
+  rimraf(join(rootPath, 'release', 'build'), (err) => {
     if (err) console.error(err)
+  })
+}
+
+if(fs.existsSync(join(rootPath, 'coverage'))) {
+  rimraf(join(rootPath, 'coverage'), (err) => {
+    if(err) console.error(err)
   })
 }
