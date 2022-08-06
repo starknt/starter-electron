a35a0a8211e1159e436cf9ce3094481d19585acc
starknt <1431880400@qq.com>
2022-08-06 22:25:47 +0800

    chore: setup simple-git-hooks script

---
+++ .knt/scripts/setup.ts
@@ -0,0 +1,16 @@
+import { join, resolve } from 'path'
+import fs from 'fs'
+import { execSync } from 'child_process'
+
+const gitDirectoryPath = resolve(process.cwd(), '.git')
+const preCommitFilePath = join(gitDirectoryPath, 'hooks', 'pre-commit')
+
+// no git
+if(!fs.existsSync(gitDirectoryPath)) {
+  process.exit(0)
+}
+
+// no setup simple-git-hooks
+if(!fs.existsSync(preCommitFilePath)) {
+  execSync('npx simple-git-hooks')
+}
