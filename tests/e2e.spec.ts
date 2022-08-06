89b359e59404237f5d32a1e88e9d581edd8ba557
starknt <1431880400@qq.com>
2022-08-06 16:26:39 +0800

    test: e2e case and test script

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,3 +1,4 @@
+import { join, resolve } from 'path'
 import type { ElectronApplication } from 'playwright'
 import { _electron as electron } from 'playwright'
 
@@ -5,13 +6,16 @@ let electronApplication: ElectronApplication
 
 beforeAll(async () => {
   // TODO: bundle file path
-  electronApplication = await electron.launch({ args: [] })
+  electronApplication = await electron.launch({ args: [join(process.cwd(), 'release', 'app', 'dist', 'main.js')], cwd: resolve(process.cwd(), 'release', 'app') })
 })
 
 afterAll(async () => {
   await electronApplication.close()
 })
 
-test('', () => {
-
+test('Main window web content', async () => {
+  const page = await electronApplication.firstWindow()
+  const element = await page.$('#app', { strict: true })
+  expect(element, 'Was unable to find the root element').toBeDefined()
+  expect((await element.innerHTML()).trim(), 'Window content was empty').not.equal('')
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
 import { alias } from './alias'
 
 export default defineConfig({
-  plugins: [
-    Vue({ reactivityTransform: true }),
-    Unocss({ configFile: 'unocss.config.ts' }),
-  ],
   resolve: {
     alias,
   },
