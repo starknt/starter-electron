3380c5dba298c49adea7b994a283129926383d24
starknt <1431880400@qq.com>
2022-08-13 13:27:52 +0800

    chore: complete setup script

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -4,6 +4,20 @@ import { execSync } from 'child_process'
 
 const gitDirectoryPath = resolve(process.cwd(), '.git')
 const preCommitFilePath = join(gitDirectoryPath, 'hooks', 'pre-commit')
+const srcElectronModulesPath = join(process.cwd(), 'app', 'electron', 'node_modules')
+const srcElectronPackagePath = join(process.cwd(), 'app', 'electron', 'package.json')
+const appPackagePath = join(process.cwd(), 'release', 'app', 'package.json')
+const appModulesPath = join(process.cwd(), 'release', 'app', 'node_modules')
+
+// if dependencies is {}, pnpm can't generate `node_modules`
+if (!fs.existsSync(appModulesPath))
+  fs.mkdirSync(appModulesPath)
+
+if (!fs.existsSync(srcElectronModulesPath) && fs.existsSync(appModulesPath))
+  fs.symlinkSync(appModulesPath, srcElectronModulesPath, 'junction')
+
+if (!fs.existsSync(srcElectronPackagePath) && fs.existsSync(appPackagePath))
+  fs.symlinkSync(appPackagePath, srcElectronPackagePath, 'file')
 
 // no git
 if (!fs.existsSync(gitDirectoryPath))
