8660c84d2de10b0bcf753a50b8eaf7fbee2baffc
starknt <1431880400@qq.com>
2022-08-15 13:34:00 +0800

    chore: complete clean script

--- scripts/clean-native.ts
+++ scripts/clean-native.ts
@@ -1,5 +1,8 @@
-import { join, resolve } from 'path'
+import { join, resolve, sep } from 'path'
 import fs from 'fs'
+import nodeAbi from 'node-abi'
+
+const { devDependencies } = JSON.parse(fs.readFileSync(join(process.cwd(), 'package.json'), 'utf-8'))
 
 const node_modules = resolve(process.cwd(), 'release', 'app', 'node_modules')
 
@@ -17,7 +20,7 @@ const modules = fs.readdirSync(node_modules, { withFileTypes: true })
   .flat(3)
   .filter(d => fs.existsSync(join(node_modules, d, 'binding.gyp')))
 
-function deleteThings(p) {
+function deleteThings(p: string) {
   if (fs.existsSync(p)) {
     fs.lstat(p, (err, stats) => {
       if (err)
@@ -31,7 +34,21 @@ function deleteThings(p) {
 
 modules.forEach((module) => {
   const p = join(node_modules, module)
-  const ReleasePath = join(p, 'build', 'Release')
+  const binPath = join(p, 'bin')
+  const buildPath = join(p, 'build')
+  const ReleasePath = join(buildPath, 'Release')
+
+  if (fs.existsSync(binPath)) {
+    const abi = nodeAbi.getAbi(/\d/.test(devDependencies.electron[0]) ? devDependencies.electron : devDependencies.electron.slice(1), 'electron')
+    if (fs.existsSync(join(binPath, `${process.platform}-${process.arch}-${abi}`)))
+      fs.rmSync(buildPath, { recursive: true, force: true })
+    fs.mkdirSync(buildPath)
+    const m = module.split(sep)
+    fs.copyFileSync(join(binPath, `${process.platform}-${process.arch}-${abi}`, `${m[m.length - 1]}.node`), join(buildPath, `${m[m.length - 1]}.node`))
+    fs.rmSync(binPath, { recursive: true, force: true })
+    return
+  }
+
   if (!fs.existsSync(join(p, 'build')) || !fs.existsSync(ReleasePath))
     return
 
