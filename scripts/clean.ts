091157423b4ca961fc781dff3a89c3ae47096467
starknt <1431880400@qq.com>
2022-08-16 09:39:54 +0800

    refactor: optimize script running speed

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -1,27 +1,32 @@
-import fs from 'fs'
 import { join } from 'path'
 import rimraf from 'rimraf'
 
 const releasePath = join(process.cwd(), 'release')
 const releaseAppPath = join(releasePath, 'app')
 
-if (fs.existsSync(join(releaseAppPath, 'dist'))) {
-  rimraf(join(releaseAppPath, 'dist'), (err) => {
-    if (err)
-      console.error(err)
+function rimrafTask(p: string) {
+  return new Promise<void>((resolve, reject) => {
+    rimraf(p, (e) => {
+      if (e)
+        reject(e)
+      resolve()
+    })
   })
 }
 
-if (fs.existsSync(join(releasePath, 'build'))) {
-  rimraf(join(releasePath, 'build'), (err) => {
-    if (err)
-      console.error(err)
-  })
+function taskFactory(paths: string[]) {
+  const r: Promise<void>[] = []
+
+  for (const task of paths)
+    r.push(rimrafTask(task))
+
+  return r
 }
 
-if (fs.existsSync(join(process.cwd(), 'coverage'))) {
-  rimraf(join(process.cwd(), 'coverage'), (err) => {
-    if (err)
-      console.error(err)
-  })
-}
+const tasks = taskFactory([
+  join(releaseAppPath, 'dist'),
+  join(releasePath, 'build'),
+  join(process.cwd(), 'coverage'),
+])
+
+Promise.allSettled(tasks)
