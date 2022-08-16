e06eaa8db2b92e3d086248c88d3412b45ca5805e
starknt <1431880400@qq.com>
2022-08-16 10:35:02 +0800

    refactor: optimize script running speed

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,5 +1,6 @@
 import { join } from 'path'
 import fs from 'fs'
+import fsPromises from 'fs/promises'
 
 const appPath = join(process.cwd(), 'app')
 const srcElectronModulesPath = join(appPath, 'electron', 'node_modules')
@@ -8,13 +9,16 @@ const releaseAppPath = join(process.cwd(), 'release', 'app')
 const appPackagePath = join(releaseAppPath, 'package.json')
 const appModulesPath = join(releaseAppPath, 'node_modules')
 
+const tasks: Promise<void>[] = []
+
 // if dependencies is {}, pnpm can't generate `node_modules`
 if (!fs.existsSync(appModulesPath))
   fs.mkdirSync(appModulesPath)
 
 if (!fs.existsSync(srcElectronModulesPath) && fs.existsSync(appModulesPath))
-  fs.symlinkSync(appModulesPath, srcElectronModulesPath, 'junction')
+  tasks.push(fsPromises.symlink(appModulesPath, srcElectronModulesPath, 'junction'))
 
 if (!fs.existsSync(srcElectronPackagePath) && fs.existsSync(appPackagePath))
-  fs.symlinkSync(appPackagePath, srcElectronPackagePath, 'file')
+  tasks.push(fsPromises.symlink(appPackagePath, srcElectronPackagePath, 'file'))
 
+Promise.allSettled(tasks)
