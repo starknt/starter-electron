6cbd34f66df7f021e09611dfa1457fa7a48ed2a3
starknt <1431880400@qq.com>
2022-11-16 12:25:31 +0800

    improve(script): set `electron-rebuild`  module-dir option

--- scripts/electron-rebuild.ts
+++ scripts/electron-rebuild.ts
@@ -1,12 +1,15 @@
 import { join } from 'path'
 import { execSync } from 'child_process'
 import fs from 'fs'
-import { appModulesPath, appPackagePath, rootPath } from './utils'
+import { appModulesPath, appPackagePath, appPath, rootPath } from './utils'
 
 const { dependencies } = JSON.parse(fs.readFileSync(appPackagePath, 'utf-8'))
 
 if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(appModulesPath)) {
-  const electronRebuildCmd = join(rootPath, 'node_modules/.bin/electron-rebuild').concat(' --force --types prod --module-dir .')
+  const electronRebuildCmd
+    = join(rootPath, 'node_modules/.bin/electron-rebuild')
+      .concat(' --force --types prod --module-dir=')
+      .concat(appPath)
 
   const cmd = process.platform === 'win32' ? electronRebuildCmd.replace(/\//, '\\') : electronRebuildCmd
 
