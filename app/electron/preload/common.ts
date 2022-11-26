971443f933c7a01c35ff27b0729c1114727fd155
starknt <1431880400@qq.com>
2022-11-26 22:51:46 +0800

    chore: midgate to `eevi` v0.5.0

--- app/electron/preload/common.ts
+++ app/electron/preload/common.ts
@@ -3,3 +3,10 @@ import { ipcRenderer } from 'electron'
 export function sayHello() {
   ipcRenderer.send('sayHello', 'hello')
 }
+
+export function sum(...nums: number[]) {
+  let s = 0
+  for (const num of nums)
+    s += num
+  return s
+}
--- app/web/pages/main.ts
+++ app/web/pages/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import { web } from 'eevi-is'
 import AppVue from '../App.vue'
+import { sum } from '#preload/common'
 import 'uno.css'
 
 // eslint-disable-next-line no-console
 console.log(web())
+
+// eslint-disable-next-line no-console
+console.log('#preload/common sum function', sum(1, 2, 3, 4, 5))
 
 const app = createApp(AppVue)
 
